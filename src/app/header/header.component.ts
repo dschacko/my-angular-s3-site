cb5f25237791905d4505909c1f5ad19a052e80d5
dschacko <76174636+dschacko@users.noreply.github.com>
2021-05-15 15:19:08 -0500

    Initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../auth/auth-service.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  public isAuthenticated = false;
+
+  /**
+   *
+   * @param authService
+   * @param router
+   */
+  constructor(private authService: AuthService, private router: Router) {
+    this.authService.authenticationEventObservable.subscribe(event => {
+      this.isAuthenticated = event;
+    });
+  }
+
+  /**
+   *
+   */
+  ngOnInit() {}
+
+  /**
+   *
+   */
+  public login() {
+    this.authService.login();
+  }
+
+  /**
+   *
+   */
+  public logout() {
+    this.authService.logout();
+    this.isAuthenticated = this.authService.isAuthenticated();
+    this.router.navigateByUrl('landing');
+  }
+}
